ec92b60b679cb3718a80c2f8aecf1840c6e7202b
otaviojava <otaviopolianasantana@gmail.com>
2020-07-04 16:55:54 -0300

    adds test

--- model/src/test/java/org/soujava/example/model/PersonTest.java
+++ model/src/test/java/org/soujava/example/model/PersonTest.java
@@ -2,13 +2,28 @@ package org.soujava.example.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class PersonTest {
 
     @Test
-    public void testJsonWriter() {
+    public void shouldCreate() {
         Person person = PersonNewInstance.newInstance();
         Assert.assertNotNull(person);
     }
 
+
+    @Test
+    public void shouldGetter() {
+        Person person = PersonNewInstance.newInstance();
+        person.setEmail("otavio");
+        Assertions.assertEquals("otavio", new AccessorPersonemail().getValue(person));
+    }
+
+    @Test
+    public void shouldSetter() {
+        Person person = PersonNewInstance.newInstance();
+        new AccessorPersonemail().setValue(person, "otavio");
+        Assertions.assertEquals("otavio", new AccessorPersonemail().getValue(person));
+    }
 }
