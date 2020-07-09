5493f531887fae62fd63a5ab662be98a23febafc
otaviojava <otaviopolianasantana@gmail.com>
2020-07-09 06:28:18 -0300

    creates class mappings

---
+++ processor/src/main/java/org/soujava/example/model/ClassMappings.java
@@ -0,0 +1,14 @@
+package org.soujava.example.model;
+
+import java.util.Optional;
+
+public interface ClassMappings {
+
+    EntityMetadata get(Class classEntity);
+
+    EntityMetadata findByName(String name);
+
+    Optional<EntityMetadata> findBySimpleName(String name);
+
+    Optional<EntityMetadata> findByClassName(String name);
+}
