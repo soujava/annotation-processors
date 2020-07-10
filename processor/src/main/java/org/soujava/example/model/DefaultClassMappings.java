726471084330397dc2ab3129b238f613cf77c2f4
otaviojava <otaviopolianasantana@gmail.com>
2020-07-10 05:29:17 -0300

    implements Default classMappings

--- processor/src/main/java/org/soujava/example/model/DefaultClassMappings.java
+++ processor/src/main/java/org/soujava/example/model/DefaultClassMappings.java
@@ -2,6 +2,7 @@ package org.soujava.example.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public final class DefaultClassMappings implements ClassMappings {
@@ -13,22 +14,36 @@ public final class DefaultClassMappings implements ClassMappings {
     }
 
     @Override
-    public EntityMetadata get(Class classEntity) {
-        return null;
+    public EntityMetadata get(Class<?> classEntity) {
+        Objects.requireNonNull(classEntity, "classEntity is required");
+        return entities.stream()
+                .filter(e -> classEntity.equals(e.getClassInstance()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("classEntity not found"));
     }
 
     @Override
     public EntityMetadata findByName(String name) {
-        return null;
+        Objects.requireNonNull(name, "name is required");
+        return entities.stream()
+                .filter(e -> name.equals(e.getName()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("classEntity not found"));
     }
 
     @Override
     public Optional<EntityMetadata> findBySimpleName(String name) {
-        return Optional.empty();
+        Objects.requireNonNull(name, "name is required");
+        return entities.stream()
+                .filter(e -> name.equals(e.getSimpleName()))
+                .findFirst();
     }
 
     @Override
     public Optional<EntityMetadata> findByClassName(String name) {
-        return Optional.empty();
+        Objects.requireNonNull(name, "name is required");
+        return entities.stream()
+                .filter(e -> name.equals(e.getClassName()))
+                .findFirst();
     }
 }
