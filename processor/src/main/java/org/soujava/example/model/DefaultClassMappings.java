2a6068fd47918f3591a3b486852000e7f3713e1c
otaviojava <otaviopolianasantana@gmail.com>
2020-07-10 05:21:36 -0300

    Updates interface method to default class mappings

--- processor/src/main/java/org/soujava/example/model/DefaultClassMappings.java
+++ processor/src/main/java/org/soujava/example/model/DefaultClassMappings.java
@@ -1,4 +1,34 @@
 package org.soujava.example.model;
 
-public class DefaultClassMappings {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class DefaultClassMappings implements ClassMappings {
+
+    private final List<EntityMetadata> entities;
+
+    public DefaultClassMappings() {
+        this.entities = new ArrayList<>();
+    }
+
+    @Override
+    public EntityMetadata get(Class classEntity) {
+        return null;
+    }
+
+    @Override
+    public EntityMetadata findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Optional<EntityMetadata> findBySimpleName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<EntityMetadata> findByClassName(String name) {
+        return Optional.empty();
+    }
 }
