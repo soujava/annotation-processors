c0cb75d58770cb0265dc0c683c179ad182de8c83
otaviojava <otaviopolianasantana@gmail.com>
2020-07-10 06:09:04 -0300

    adds qualified to class mappings model

--- processor/src/main/java/org/soujava/example/model/ClassMappingsModel.java
+++ processor/src/main/java/org/soujava/example/model/ClassMappingsModel.java
@@ -1,4 +1,20 @@
 package org.soujava.example.model;
 
+import java.util.List;
+
 public class ClassMappingsModel {
+
+    private final List<String> entities;
+
+    public ClassMappingsModel(List<String> entities) {
+        this.entities = entities;
+    }
+
+    public List<String> getEntities() {
+        return entities;
+    }
+
+    public String getQualified() {
+        return "org.soujava.example.model.DefaultClassMappings";
+    }
 }
