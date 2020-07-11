f86b530af9160a87de377bf32b02dc15921f723d
otaviojava <otaviopolianasantana@gmail.com>
2020-07-11 06:00:27 -0300

    adds tests

--- model/src/test/java/org/soujava/example/model/PersonTest.java
+++ model/src/test/java/org/soujava/example/model/PersonTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Map;
+
 public class PersonTest {
 
     private ClassMappings mappings = new DefaultClassMappings();
@@ -15,12 +17,14 @@ public class PersonTest {
         Assert.assertNotNull(person);
     }
 
-
     @Test
     public void shouldGetter() {
-        Person person = new PersonEntityMetaData().newInstance();
-        person.setEmail("otavio");
-        Assertions.assertEquals("otavio", new PersonEmailFieldMetaData().read(person));
+        final EntityMetadata entityMetadata = mappings.get(Person.class);
+        final Map<String, FieldMetadata> fieldsGroupByName = entityMetadata.getFieldsGroupByName();
+        final FieldMetadata fieldMetadata = fieldsGroupByName.get("native");
+        Person person = entityMetadata.newInstance();
+        fieldMetadata.write(person, "native");
+        Assertions.assertEquals(person.getUsername(), fieldMetadata.read(person));
     }
 
     @Test
