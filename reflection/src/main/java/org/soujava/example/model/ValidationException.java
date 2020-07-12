c92a0b5168de65e05e27b38b001c80ce2ccded4a
otaviojava <otaviopolianasantana@gmail.com>
2020-07-12 12:31:49 -0300

    updates reflection api

---
+++ reflection/src/main/java/org/soujava/example/model/ValidationException.java
@@ -0,0 +1,12 @@
+package org.soujava.example.model;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
