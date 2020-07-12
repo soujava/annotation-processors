70ec6ef41b40b639033cc6b9fadfd723cf0f185d
otaviojava <otaviopolianasantana@gmail.com>
2020-07-12 15:02:30 -0300

    updates test

--- reflection/src/main/java/org/soujava/metadata/reflection/ReflectionMapper.java
+++ reflection/src/main/java/org/soujava/metadata/reflection/ReflectionMapper.java
@@ -1,4 +1,23 @@
 package org.soujava.metadata.reflection;
 
-public class ReflectionMapper implements  {
+import org.soujava.medatadata.api.Mapper;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class ReflectionMapper implements Mapper {
+
+    @Override
+    public <T> T toEntity(Map<String, Object> map, Class<T> type) {
+        Objects.requireNonNull(map, "Map is required");
+        Objects.requireNonNull(type, "type is required");
+
+        return null;
+    }
+
+    @Override
+    public <T> Map<String, Object> toMap(T entity) {
+        Objects.requireNonNull(entity, "entity is required");
+        return null;
+    }
 }
