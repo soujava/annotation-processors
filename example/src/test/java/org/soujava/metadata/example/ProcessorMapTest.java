b5fb785157cfff4570795ef9d0f172739069eca0
otaviojava <otaviopolianasantana@gmail.com>
2020-07-12 14:47:52 -0300

    adds mapp test

--- example/src/test/java/org/soujava/metadata/example/ProcessorMapTest.java
+++ example/src/test/java/org/soujava/metadata/example/ProcessorMapTest.java
@@ -7,6 +7,7 @@ import org.soujava.medatadata.api.Mapper;
 import org.soujava.metadata.processor.ProcessorMap;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProcessorMapTest {
@@ -34,10 +35,18 @@ public class ProcessorMapTest {
 
     }
 
-
-
     @Test
     public void shouldCreateEntity() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 10L);
+        map.put("email","otaviojava@java.net");
+        map.put("native","otaviojava");
+        map.put("contacts",Arrays.asList("Poliana", "Bruno Souza", "Yanaga", "Elder"));
 
+        final Person person = mapper.toEntity(map, Person.class);
+        Assertions.assertEquals(10L, person.getId());
+        Assertions.assertEquals("otaviojava", person.getUsername());
+        Assertions.assertEquals("otaviojava@java.net", person.getEmail());
+        Assertions.assertEquals(person.getContacts(), person.getContacts());
     }
 }
