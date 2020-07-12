81b2ef9c42331e0ea0dc7117c42bae67f49cfa74
otaviojava <otaviopolianasantana@gmail.com>
2020-07-12 12:31:39 -0300

    adds example module

---
+++ example/src/main/java/org/soujava/example/model/Animal.java
@@ -0,0 +1,27 @@
+package org.soujava.example.model;
+
+@Entity("kind")
+public class Animal {
+
+    @Id
+    private String name;
+
+    @Column
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+}
