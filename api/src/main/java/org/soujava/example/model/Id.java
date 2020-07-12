b08350bdabdee80bffd0a2a4a7b843d55bc7e40b
otaviojava <otaviopolianasantana@gmail.com>
2020-07-12 12:27:48 -0300

    adds add api module

---
+++ api/src/main/java/org/soujava/example/model/Id.java
@@ -0,0 +1,12 @@
+package org.soujava.example.model;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+public @interface Id {
+
+    String value() default "";
+}
