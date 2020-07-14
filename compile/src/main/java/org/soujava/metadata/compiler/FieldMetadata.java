3d2d3e4f8704a1387f2728910b57be70d00eca14
otaviojava <otaviopolianasantana@gmail.com>
2020-07-14 06:04:43 -0300

    creates field metadata

--- compile/src/main/java/org/soujava/metadata/compiler/FieldMetadata.java
+++ compile/src/main/java/org/soujava/metadata/compiler/FieldMetadata.java
@@ -1,4 +1,39 @@
 package org.soujava.metadata.compiler;
 
+import java.lang.reflect.Field;
+
 public class FieldMetadata {
+
+    private final Field field;
+
+    private final FieldReader reader;
+
+    private final FieldWriter writer;
+
+    FieldMetadata(Field field, FieldReader reader, FieldWriter writer) {
+        this.field = field;
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public FieldReader getReader() {
+        return reader;
+    }
+
+    public FieldWriter getWriter() {
+        return writer;
+    }
+
+    @Override
+    public String toString() {
+        return "FieldMetadata{" +
+                "field=" + field +
+                ", reader=" + reader +
+                ", writer=" + writer +
+                '}';
+    }
 }
