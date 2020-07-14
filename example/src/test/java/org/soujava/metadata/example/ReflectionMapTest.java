70f7686b6303ff4dec73da2a98152e6ea447bbd3
otaviojava <otaviopolianasantana@gmail.com>
2020-07-14 19:38:53 -0300

    adds reflection map test

--- example/src/test/java/org/soujava/metadata/example/ReflectionMapTest.java
+++ example/src/test/java/org/soujava/metadata/example/ReflectionMapTest.java
@@ -1,13 +1,13 @@
 package org.soujava.metadata.example;
 
 import org.soujava.medatadata.api.Mapper;
-import org.soujava.metadata.processor.ProcessorMap;
+import org.soujava.metadata.reflection.ReflectionMapper;
 
 public class ReflectionMapTest extends AbstractMapperTest {
 
     @Override
     protected Mapper getMapper() {
-        return new ProcessorMap();
+        return new ReflectionMapper();
     }
 
 
