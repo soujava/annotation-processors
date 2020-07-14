749437e4ad8d7ccb67ae76dbf7dde9776bc4609b
otaviojava <otaviopolianasantana@gmail.com>
2020-07-14 06:17:36 -0300

    creates entity metadata factory

--- compile/src/main/java/org/soujava/metadata/compiler/CompileMapper.java
+++ compile/src/main/java/org/soujava/metadata/compiler/CompileMapper.java
@@ -4,19 +4,39 @@ import org.soujava.medatadata.api.Mapper;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class CompileMapper implements Mapper {
 
-    private Map<Class<?>, Object> mapper = new HashMap<>();
-
+    private Map<Class<?>, EntityMetadata> mapper = new HashMap<>();
+    private final EntityMetadataFactory factory = new EntityMetadataFactory();
 
     @Override
     public <T> T toEntity(Map<String, Object> map, Class<T> type) {
+        Objects.requireNonNull(map, "Map is required");
+        Objects.requireNonNull(type, "type is required");
+
+        final EntityMetadata entityMetadata = getEntityMetadata(type);
+        entityMetadata.getFields();
         return null;
     }
 
     @Override
     public <T> Map<String, Object> toMap(T entity) {
+        Objects.requireNonNull(entity, "entity is required");
+        final EntityMetadata entityMetadata = getEntityMetadata(entity.getClass());
         return null;
+    }
+
+    private <T> EntityMetadata getEntityMetadata(Class<T> type) {
+        final EntityMetadata entityMetadata = mapper.get(type);
+        if (entityMetadata != null) {
+            return entityMetadata;
+        }
+        synchronized (type) {
+            final EntityMetadata entity = factory.apply(type);
+            mapper.put(type, entity);
+            return entity;
+        }
     }
 }
---
+++ compile/src/main/java/org/soujava/metadata/compiler/EntityMetadataFactory.java
@@ -0,0 +1,12 @@
+package org.soujava.metadata.compiler;
+
+import java.util.function.Function;
+
+public class EntityMetadataFactory implements Function<Class<?>, EntityMetadata> {
+
+    @Override
+    public EntityMetadata apply(Class<?> type) {
+
+        return null;
+    }
+}
