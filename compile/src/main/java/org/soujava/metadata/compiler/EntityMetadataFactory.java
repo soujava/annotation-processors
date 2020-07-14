f6862d9a1811e4447bc4e01f2ed899eb7fb692e7
otaviojava <otaviopolianasantana@gmail.com>
2020-07-14 07:10:15 -0300

    creates entity metadata facatory

--- compile/src/main/java/org/soujava/metadata/compiler/EntityMetadataFactory.java
+++ compile/src/main/java/org/soujava/metadata/compiler/EntityMetadataFactory.java
@@ -4,6 +4,8 @@ import org.soujava.medatadata.api.Column;
 import org.soujava.medatadata.api.Id;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 class EntityMetadataFactory implements Function<Class<?>, EntityMetadata> {
@@ -21,13 +23,14 @@ class EntityMetadataFactory implements Function<Class<?>, EntityMetadata> {
     @Override
     public EntityMetadata apply(Class<?> type) {
 
+        List<FieldMetadata> fields = new ArrayList<>();
         for (Field field : type.getDeclaredFields()) {
             final Id id = field.getAnnotation(Id.class);
             final Column column = field.getAnnotation(Column.class);
             if (id != null || column != null) {
                 final FieldReader reader = readerFactory.apply(field);
                 final FieldWriter writer = writerFactory.apply(field);
-                final FieldMetadata metadata = new FieldMetadata(field, reader, writer);
+                fields.add(new FieldMetadata(field, reader, writer));
             }
         }
         return null;
