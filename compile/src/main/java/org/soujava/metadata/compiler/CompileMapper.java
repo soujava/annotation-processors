bd4bae7f13d5ed24260b0cebb67ab82edeaad582
otaviojava <otaviopolianasantana@gmail.com>
2020-07-14 06:05:03 -0300

    adds a mapper implementation to compile

--- compile/src/main/java/org/soujava/metadata/compiler/CompileMapper.java
+++ compile/src/main/java/org/soujava/metadata/compiler/CompileMapper.java
@@ -1,4 +1,22 @@
 package org.soujava.metadata.compiler;
 
-public class CompileMapper {
+import org.soujava.medatadata.api.Mapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompileMapper implements Mapper {
+
+    private Map<Class<?>, Object> mapper = new HashMap<>();
+
+
+    @Override
+    public <T> T toEntity(Map<String, Object> map, Class<T> type) {
+        return null;
+    }
+
+    @Override
+    public <T> Map<String, Object> toMap(T entity) {
+        return null;
+    }
 }
