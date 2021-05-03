2d31ad5d4234708ade75ba43184ec584c40e4a5a
Otavio Santana <otaviopolianasantana@gmail.com>
2021-05-03 16:08:32 +0100

    creates person entity

---
+++ reflection/src/test/java/org/soujava/metadata/reflection/Person.java
@@ -0,0 +1,46 @@
+package org.soujava.metadata.reflection;
+
+
+import org.soujava.medatadata.api.Column;
+import org.soujava.medatadata.api.Entity;
+import org.soujava.medatadata.api.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private final String id;
+
+    @Column
+    private final  String name;
+
+    @Column
+    private final  String country;
+
+    public Person(String id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
