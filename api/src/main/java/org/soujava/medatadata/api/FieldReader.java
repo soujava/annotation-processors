bbadfec629c828cf6625acd1dac3235657c4faf1
Otavio Santana <otaviopolianasantana@gmail.com>
2021-05-04 07:09:53 +0100

    creates field reader

---
+++ api/src/main/java/org/soujava/medatadata/api/FieldReader.java
@@ -0,0 +1,48 @@
+package org.soujava.medatadata.api;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public interface FieldReader {
+
+    <T> void read(T entity, Map<String, Object> map, Field field) throws IllegalAccessException;
+
+
+    class IdFieldReader implements FieldReader {
+
+        @Override
+        public <T> void read(T entity, Map<String, Object> map, Field field) throws IllegalAccessException {
+            final Id id = field.getAnnotation(Id.class);
+            final String fieldName = field.getName();
+            String idName = id.value().isBlank() ? fieldName : id.value();
+            field.setAccessible(true);
+            final Object value = field.get(entity);
+            map.put(idName, value);
+        }
+    }
+
+    class ColumnFieldReader implements FieldReader {
+
+        @Override
+        public <T> void read(T entity, Map<String, Object> map, Field field) throws IllegalAccessException {
+            final Column column = field.getAnnotation(Column.class);
+            final String fieldName = field.getName();
+            String columnName = column.value().isBlank() ? fieldName : column.value();
+            field.setAccessible(true);
+            final Object value = field.get(entity);
+            map.put(columnName, value);
+        }
+    }
+
+    class NoopFieldReader implements FieldReader {
+
+        @Override
+        public <T> void read(T entity, Map<String, Object> map, Field field) throws IllegalAccessException {
+
+        }
+    }
+
+    static FieldReader of(Field field) {
+        return field.getAnnotation(Id.class) ? null
+    }
+}
